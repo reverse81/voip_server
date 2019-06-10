c42c94c4b79064371673c5adaba7e91ddba6dd0e
miheui seok <miheui.seok@lge.com>
2019-06-10 16:36:22 -0400

    add module for data and environment setting

---
+++ data/dataCrypto.js
@@ -0,0 +1,29 @@
+
+var database = require("./database")
+var mycrypto = require('../lib/cryptoAlgorithms')
+
+
+module.exports = {
+  findUser: function(data){
+    console.log("findUser");
+    for(var n in data){
+      data[n] = mycrypto.encrypt("SHA256", data[n]);
+    }
+    console.log(data);
+    return database.findUser(data);
+  },
+  deleteUser: function(data){
+
+    return database.deleteUser(data);
+  },
+  saveUser: function(data){
+    data.email=mycrypto.encrypt("SHA256", data.email);
+    data.pwd=mycrypto.encrypt("SHA256", data.pwd);
+    console.log(data);
+    return database.saveUser(data);
+  },
+  findUserId: function(data){
+    return database.findUserId(data);
+  }
+
+}
---
+++ data/database.js
@@ -0,0 +1,68 @@
+
+
+var mongoClient = require('mongodb').MongoClient;
+
+var database = {
+  "users": null,
+  "schedules": null
+};
+//{ "_id" : "rid@naver.com", "email" : "rid@naver.com", "pwd" : "1234", "phone" : "010-1234-5678", "ip" : "0.0.0.1" }
+
+module.exports = {
+  connectDB: function(){
+      var databaseURL = 'mongodb://127.0.0.1:27017/VoIP';
+      mongoClient.connect(databaseURL,
+          function (err, cluster)
+          {
+              if (err) {
+                  console.log('db connect error');
+                  return;
+              }
+              console.log('db was connected : ' + databaseURL);
+              var temp = cluster.db("VoIP");
+              database.users = temp.collection('users');
+              database.schedules = temp.collection('schedules');
+              const collection = cluster.db("VoIP").collection("users");
+          }
+      );
+  },
+  findUser: function(data){
+    return new Promise(function (resolve, reject){
+      database.users.findOne(data, function(err, result){
+        if(err) throw err;
+        resolve(result);
+      })
+    })
+  },
+
+  deleteUser: function(data){
+    database.users.deleteOne(data, function(err, result){
+      if(err) throw err;
+      console.log("deleted");
+    });
+  },
+  saveUser: function(data){
+    return new Promise(function (resolve, reject){
+      database.users.insertOne(data, function(err, result){
+        if(err) throw err;
+        console.log("inserted");
+        resolve(result);
+      })
+    })
+
+  },
+  findUserId: function(data){
+    return new Promise(function (resolve, reject){
+      database.users.stats(function(err, result){
+        database.users.find({}).toArray(function(err, re){
+          next_id = re[result.count-1]._id+1;
+          resolve(next_id);
+        })
+      })
+    })
+  },
+  updateUser: function(email, data){
+    database.users.findOneAndUpdate(email, data)
+  }
+
+}
